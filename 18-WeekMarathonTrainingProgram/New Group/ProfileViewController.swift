579067b9ac5ef9daf70f5546379746b72491ad67
PinYuanChen <champion790713@gmail.com>
2019-03-24 14:46:46 +0800

    fix fb login problem able to login and logout
    https://github.com/PinYuanChen/FirstLog.git

--- 18-WeekMarathonTrainingProgram/New Group/ProfileViewController.swift
+++ 18-WeekMarathonTrainingProgram/New Group/ProfileViewController.swift
@@ -13,13 +13,12 @@ import FBSDKLoginKit
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var avatarImageView: UIImageView!
-    
     @IBOutlet weak var downloadUploadStackView: UIStackView!
     @IBOutlet weak var downloadRecordButton: UIButton!
     @IBOutlet weak var uploadRecordButton: UIButton!
     @IBOutlet weak var loginStackView: UIStackView!
+    @IBOutlet weak var logoutButton: UIButton!
     @IBOutlet weak var loginFBButton: UIButton!
-    @IBOutlet weak var createAccountButton: UIButton!
     var isLogin = false
     
     override func viewDidLoad() {
@@ -27,20 +26,53 @@ class ProfileViewController: UIViewController {
         navigationSetUp(target: self)
         self.navigationItem.title = "Profile"
         // Do any additional setup after loading the view.
-        loginStackView.isHidden = isLogin ? true:false
-        downloadUploadStackView.isHidden = isLogin ? false:true
+        reloadData()
     }
     
+    func fetchProfile() {
+        FBSDKGraphRequest(graphPath: "me", parameters: ["fields" : "email, first_name, last_name, id, gender, picture.type(large)"])
+            .start(completionHandler:  {
+                (connection, result, error) in
+                
+                guard
+                    let result = result as? NSDictionary,
+                    let picture = result["picture"]  as? NSDictionary,
+                    let data = picture["data"] as? NSDictionary,
+                    let picURL = data["url"] as? String
+                    else {
+                        return
+                }
+                if let userImageURL = URL(string: picURL) {
+                    if let userImageData = NSData(contentsOf: userImageURL) as Data? {
+                        self.isLogin = true
+                        self.avatarImageView.image = UIImage(data: userImageData)
+                        self.avatarImageView.layer.cornerRadius = 50
+                        self.avatarImageView.layer.masksToBounds = true
+                        self.loginStackView.isHidden = self.isLogin
+                        self.downloadUploadStackView.isHidden = !self.isLogin
+                    }
+                }
+            })
+    }
 
    
     @IBAction func didTappedLoginWithFBButton(_ sender: UIButton) {
         facebookLogin(sender: sender)
     }
     
-    
-    @IBAction func didTappedCreateAccountButton(_ sender: Any) {
+    @IBAction func didTappedLogoutButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Logout", message: "Are you sure you want to logout?", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Confirm", style: .default) { _ in
+            FBSDKLoginManager().logOut()
+            self.isLogin = false
+            self.reloadData()
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(okayAction)
+        alertController.addAction(cancel)
+        self.present(alertController, animated: true, completion: nil)
+        
     }
-    
     
     @IBAction func didTappedDownloadRecordButton(_ sender: Any) {
     }
@@ -48,10 +80,22 @@ class ProfileViewController: UIViewController {
     @IBAction func didTappedUploadRecordButton(_ sender: Any) {
     }
     
+    func reloadData () {
+        loginStackView.isHidden = isLogin
+        downloadUploadStackView.isHidden = !isLogin
+        
+        if FBSDKAccessToken.current() != nil {
+            fetchProfile()
+        }else{
+            avatarImageView.image = UIImage(named: "avatarPlaceholder")
+        }
+    }
+    
     //MARK: - FB login
     func facebookLogin(sender: UIButton) {
-        let fbLoginManager = FBSDKLoginManager()
-        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+        
+        FBSDKLoginManager().logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+            
             if let error = error {
                 print("Failed to login: \(error.localizedDescription)")
                 return
@@ -75,14 +119,10 @@ class ProfileViewController: UIViewController {
                     
                     return
                 }
-                
-                // TODO: modify this
-                // Present the main view
-                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
-                    UIApplication.shared.keyWindow?.rootViewController = viewController
-                    self.dismiss(animated: true, completion: nil)
+                DispatchQueue.main.async {
+                    self.isLogin = true
+                    self.reloadData()
                 }
-                
             })
             
         }
